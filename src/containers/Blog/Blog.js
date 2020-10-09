d5e216a4642a0f271c1c232c42025dbf1af2cfa2
GeoFlev <geoflev@hotmail.com>
2020-10-09 18:56:26 +0300

    routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,68 +1,72 @@
-import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
+import React, { Component } from "react";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+import "./Blog.css";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import asyncComponent from "../../hoc/AsyncComponent";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+const AsyncNewPost = asyncComponent(() => {
+  //dynamic import when NewPost component needed
+  return import("./NewPost/NewPost");
+});
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount () {
-        axios.get( '/posts' )
-            .then( response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                // console.log( response );
-            } )
-            .catch(error => {
-                // console.log(error);
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
-
-        return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    auth: true,
+  };
+  //NavLink is the same as Link but has some aditions f.e. the class so we can make
+  // 1 link active
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/posts"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/** switch to always pick one to render */}
+        <Switch>
+          {/* to conditionally render a page if auth is needed*/}
+          {this.state.auth ? (
+            <Route path="/new-post" component={AsyncNewPost} />
+          ) : null}
+          {/*<Route path="/new-post" component={NewPost} />*/}
+          <Route path="/posts" component={Posts} />
+          {/* 2nd way for managing 404, should always be last */}
+          <Route render={() => <h1>Not Found</h1>} />
+          {/*
+            catches all unknown pages and redirects, so no 404
+            1st solution to this
+        <Redirect from="/" to="/posts" />*/}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Blog;
